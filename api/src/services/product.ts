0f8be5df7d3100c47e424aa7e966a2ca9df5d150
Mateo Quintero <matteoquintero@gmail.com>
2022-09-29 03:57:27 -0500

    feact(categories): limit categories

--- api/src/services/product.ts
+++ api/src/services/product.ts
@@ -25,11 +25,33 @@ export const getProducts = (filter:string):Promise<responseProducts> => {
                     picture: product.thumbnail || null,
                     condition: product.condition || null,
                     free_shipping: product.shipping.free_shipping || null,
-                    address: Object.values(product.address).join(' ') || null
+                    address: product.address.state_name +' '+ product.address.city_name || null,
+                    price:{
+                        currency: product.currency_id || null,
+                        amount: product.price || null,
+                        decimals: 0 //I can't find this value in api
+                    }                    
 			}));
 
+
             //Map and filter data to create list of categories
-            if(  data.filters.find( (data: { id: string; }) => data.id == 'category')){
+            //
+            //in the test information it did not say what parameter 
+            //to take the categories for that reason the repeated 
+            //code of filters and available_filters
+            if( data.available_filters.find( (data: { id: string; }) => data.id == 'category')){
+                categories = data.available_filters.find( 
+                    (data: { id: string; }) => data.id == 'category'
+                )
+                .values
+                //Filter to reduce breadcrumbs
+                .filter(
+                    (category: any) => { return category.results > 1800 }
+                )
+                .map(
+                    (category: any) => { return category.name }
+                )
+            } else if( data.filters.find( (data: { id: string; }) => data.id == 'category')){
                 categories = data.filters.find( 
                     (data: { id: string; }) => data.id == 'category'
                 )
@@ -61,25 +83,28 @@ export const getProduct = (id:string):Promise<responseProduct> => {
         item: undefined
     }    
     return new Promise((resolve, reject) => {
+        //Array of promises to get product detail
         Promise.all([
             fetchData(`https://api.mercadolibre.com/items/${id}`),
             fetchData(`https://api.mercadolibre.com/items/${id}/description`)
         ]).then(function (responses) {
+            //Map of promises to create response
             return Promise.all(responses.map(function (response) {
                 return response;
             }));
         }).then(function (data:any) {
+            //Map data to create detail of product
             productDetail = {
-                id: data[0].id,
-                title: data[0].title,
-                picture: data[0].thumbnail,
-                condition: data[0].condition,
-                free_shipping: data[0].shipping.free_shipping,
-                sold_quantity: data[0].sold_quantity,
-                description: data[1].plain_text,
+                id: data[0].id || null,
+                title: data[0].title || null,
+                picture: data[0].thumbnail || null,
+                condition: data[0].condition || null,
+                free_shipping: data[0].shipping.free_shipping || null,
+                sold_quantity: data[0].sold_quantity || null,
+                description: data[1].plain_text || null,
                 price:{
-                    currency: data[0].currency_id,
-                    amount: data[0].price,
+                    currency: data[0].currency_id || null,
+                    amount: data[0].price || null,
                     decimals: 0
                 }
             }
@@ -87,7 +112,7 @@ export const getProduct = (id:string):Promise<responseProduct> => {
             resolve(responseProduct)
         }).catch(function (error) {
             console.log({error});
-            reject(true)
+            reject(error)
         });
     });
 
